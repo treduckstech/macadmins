b7cd9e964945607e42ae9c356a1d9c7b60fc31cc
Ben Rillie <ben@treducks.tech>
2025-05-13 08:31:09 -0700

    removed zero day check

--- Kandji/Daily Checks/latestOScheck.py
+++ Kandji/Daily Checks/latestOScheck.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# apollo-sync
 
 import requests
 import os
@@ -26,13 +27,11 @@ def get_latest_versions_from_json(json_url):
     latest_build = latest_info['Build']
     latest_release_date = datetime.fromisoformat(latest_info['ReleaseDate'].replace("Z", "+00:00"))
     formatted_release_date = latest_release_date.strftime('%B %d, %Y')
-    zero_day_exploits = latest_info.get('ActivelyExploitedCVEs', [])
-
-    return latest_version, latest_build, formatted_release_date, zero_day_exploits, latest_release_date
+    return latest_version, latest_build, formatted_release_date, latest_release_date
 
 # Fetch the latest iOS and macOS versions
-latest_ios_version, latest_ios_build, latest_ios_release_date, ios_zero_days, ios_release_datetime = get_latest_versions_from_json(ios_json_url)
-latest_macos_version, latest_macos_build, latest_macos_release_date, macos_zero_days, macos_release_datetime = get_latest_versions_from_json(macos_json_url)
+latest_ios_version, latest_ios_build, latest_ios_release_date, ios_release_datetime = get_latest_versions_from_json(ios_json_url)
+latest_macos_version, latest_macos_build, latest_macos_release_date, macos_release_datetime = get_latest_versions_from_json(macos_json_url)
 
 # Calculate days since release
 current_date = datetime.now(timezone.utc)
@@ -45,14 +44,14 @@ print(f"  Version: {latest_macos_version}")
 print(f"  Build: {latest_macos_build}")
 print(f"  Release Date: {latest_macos_release_date}")
 print(f"  Days Since Release: {macos_days_since_release}")
-print(f"  Zero Day Exploits: {'None' if not macos_zero_days else ', '.join(macos_zero_days)}\n")
+
 
 print("iOS:")
 print(f"  Version: {latest_ios_version}")
 print(f"  Build: {latest_ios_build}")
 print(f"  Release Date: {latest_ios_release_date}")
 print(f"  Days Since Release: {ios_days_since_release}")
-print(f"  Zero Day Exploits: {'None' if not ios_zero_days else ', '.join(ios_zero_days)}\n")
+
 
 # Complete API URL with path and query parameters
 api_url = f"{base_url}/api/v1/devices?limit=300"
@@ -114,10 +113,10 @@ message = (
     "Latest OS Versions:\n"
     f"macOS:\n  Version: {latest_macos_version}\n  Build: {latest_macos_build}\n  Release Date: {latest_macos_release_date}\n"
     f"  Days Since Release: {macos_days_since_release}\n"
-    f"  Zero Day Exploits: {'None' if not macos_zero_days else ', '.join(macos_zero_days)}\n\n"
+    ""
     f"iOS:\n  Version: {latest_ios_version}\n  Build: {latest_ios_build}\n  Release Date: {latest_ios_release_date}\n"
     f"  Days Since Release: {ios_days_since_release}\n"
-    f"  Zero Day Exploits: {'None' if not ios_zero_days else ', '.join(ios_zero_days)}\n\n"
+    ""
     "Devices not running the latest OS:\n"
 )
 
